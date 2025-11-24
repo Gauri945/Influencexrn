29b8772ce63f427740eb50bef8e13eae7a163303
Gauri_Chauhan <cgauri915@gmail.com>
2025-11-24 10:36:29 +0530

    Fix import statement formatting in Collaboration.js

--- models/Collaboration.js
+++ models/Collaboration.js
@@ -1,4 +1,4 @@
-import mongoose from 'mongoose';
+import mongoose from 'mongoose';  //require 
 
 const collaborationSchema = mongoose.Schema({
   influencer_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Influencer', required: true },
